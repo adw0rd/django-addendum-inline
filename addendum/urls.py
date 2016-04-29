88456921ac55b4816ac3edd0e4f76ff3417580e0
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2016-04-29 20:57:19 +0300

    Fixed urls.py for Django>1.8

--- addendum/urls.py
+++ addendum/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import patterns, url
+from django import VERSION as DJANGO_VERSION
 
 from .views import InlineSaveView
+
+if DJANGO_VERSION[:2] >= (1, 8):
+    from django.conf.urls import url
+    patterns = lambda *args: [a for a in args if a]
+else:
+    from django.conf.urls import patterns, url
+
 
 
 urlpatterns = patterns(
